11699580e411b5483f5a23008c01bac7b92e12ca
Francesco Frosini <ott8bre@gmail.com>
2015-11-15 23:13:16 +0100

    refactoring

--- xml.js
+++ xml.js
@@ -1,55 +1,60 @@
-function xml(tag, arg1, arg2){
-    if(arguments.length < 1 || !tag || typeof tag !== 'string') return;
+(function(global){
 
-    var attrs_str = '';
-    var inner_str = ''
+  function is_scalar(obj){return (/string|number|boolean/).test(typeof obj);}
 
-    function is_scalar(obj){return (/string|number|boolean/).test(typeof obj);}
+  function concat(array){
+      var str = '';
+      for(var a in array){
+          var v = array[a];
+          if(v!==null && v!==undefined) {
+              str += array[a];
+          }
+      }
+      return str;
+  }
 
-    function concat(array){
-        var str = '';
-        for(var a in array){
-            var v = array[a];
-            if(v!==null && v!==undefined) {
-                str += array[a];
-            }
-        }
-        return str;
-    }
+  function flat(object){
+      var str='';
+      for(var a in object){
+          str += ' '+a+'='+'"'+object[a]+'"';
+      }
+      return str;
+  }
 
-    function flat(object){
-        var str='';
-        for(var a in object){
-            str += ' '+a+'='+'"'+object[a]+'"';
-        }
-        return str;
-    }
+  function xml(tag, arg1, arg2){
+      if(arguments.length < 1 || !tag || typeof tag !== 'string') return;
 
-    if(arg1!==undefined){
-        if(typeof arg1 === 'object'){
-            if(arg1.constructor == Array){
-                // arg1 array
-                inner_str = concat(arg1);
-            } else {
-                // arg1 object
+      var attrs_str = '';
+      var inner_str = '';
 
-                attrs_str = flat(arg1);
+      if(arg1!==undefined){
+          if(typeof arg1 === 'object'){
+              if(arg1.constructor == Array){
+                  // arg1 array
+                  inner_str = concat(arg1);
+              } else {
+                  // arg1 object
 
-                if(arg2!==undefined){
-                    if(arg2.constructor == Array){
-                        // arg2 array
-                        inner_str = concat(arg2);
-                    }else if(is_scalar (arg2)){
-                        // arg2 string
-                        inner_str = arg2;
-                    }
-                }
-            }
-        } else if(is_scalar (arg1)){
-            // arg1 string
-            inner_str = arg1;
-        }
-    }
+                  attrs_str = flat(arg1);
 
-    return "<" + tag + attrs_str + ">" + (inner_str) + "</" + tag + ">";
-}
+                  if(arg2!==undefined){
+                      if(arg2.constructor == Array){
+                          // arg2 array
+                          inner_str = concat(arg2);
+                      }else if(is_scalar (arg2)){
+                          // arg2 string
+                          inner_str = arg2;
+                      }
+                  }
+              }
+          } else if(is_scalar (arg1)){
+              // arg1 string
+              inner_str = arg1;
+          }
+      }
+
+      return "<" + tag + attrs_str + ">" + (inner_str) + "</" + tag + ">";
+  }
+
+  global.xml = xml;
+})(this);
